f323e79d93048655f7ca21fbbc47dcb766fac20a
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-07-08 14:01:55 +0100

    Add index.js file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+import Task from './modules/crud';
+
+
+const todoItemsList = document.querySelector('#list_navv');
+const clearAllBtn = document.querySelector('#clearAllBTN');
+const task = new Task();
+task.addTask();
+task.render();
+todoItemsList.addEventListener('click', (event) => {
+  if (event.target.type === 'checkbox') {
+    toggle(event.target.parentElement.getAttribute('data-key'), task);
+    task.render();
+  }
+});
+clearAllBtn.addEventListener('click', () => {
+  clearAll(task);
+  task.render();
+});
