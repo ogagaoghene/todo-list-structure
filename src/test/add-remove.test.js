360870cb2d17cc3684c35f31ffe7a07a1e499f7f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-07-12 11:20:14 +0100

    Add add-remove.test.js file

---
+++ src/test/add-remove.test.js
@@ -0,0 +1,34 @@
+import { displayTasks, removeTask } from '../modules/utils';
+import Task from '../modules/tasks';
+
+const Tasks = new Task();
+
+document.body.innerHTML = '<ul class="nav__items"></ul>';
+const listContainer = document.querySelector('.nav__items');
+
+describe('Test Add Task and local storage', () => {
+  window.localStorage = Storage.prototype;
+  test('Add new Task', () => {
+    const task = {
+      index: 0,
+      description: 'This is sample test',
+      completed: false,
+    };
+    Tasks.addTask(task); 
+    displayTasks(Tasks.tasksArray, listContainer);
+    expect(Tasks.tasksArray).toHaveLength(1); 
+  });
+
+  test('Test local storage', () => {
+    JSON.parse(localStorage.getItem('todos'));
+    expect(localStorage).toHaveLength(1);
+  });
+});
+
+describe('Test Remove task from list', () => {
+  window.localStorage = Storage.prototype;
+  test('Remove task', () => {
+    removeTask(Tasks.tasksArray, Tasks.addTask, '0');
+    expect(Tasks.tasksArray).toHaveLength(0);
+  });
+});
